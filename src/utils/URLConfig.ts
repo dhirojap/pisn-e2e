b4568347fbddacb54e9bf4e52ebd05b29ea9185c
DhiroJap <dhirojap@gmail.com>
2024-06-03 16:06:49 +0700

    refactor url config

--- src/utils/URLConfig.ts
+++ src/utils/URLConfig.ts
@@ -1,28 +1,37 @@
+import { Role } from "./UserParse";
+
 interface IMissingBaseURLError extends Error {
   message: string;
 }
 
-function getBaseURL(): string {
-  if (!process.env.BASE_URL) {
-    throw new Error(
-      "Missing BASE_URL environment variable. Please check your .env file!"
-    ) as IMissingBaseURLError;
+class URLConfig {
+  private readonly baseURL: string;
+
+  constructor() {
+    this.baseURL = this.getBaseURL();
   }
 
-  return process.env.BASE_URL;
+  getBaseURL(): string {
+    if (!process.env.BASE_URL) {
+      throw new Error(
+        "Missing BASE_URL environment variable. Please check your .env file!"
+      ) as IMissingBaseURLError;
+    }
+
+    return process.env.BASE_URL;
+  }
+
+  getEligibilityURL(role: Role): string {
+    return `${this.baseURL}/${role}/check-eligibility`;
+  }
+
+  getGenerateURL(role: Role): string {
+    return `${this.baseURL}/${role}/generated-certificate`;
+  }
 }
-
-const baseURL = getBaseURL();
-console.log(process.env.BASE_URL);
-
-interface IURLConfig {
-  baseURL: string;
-  loginURL: string;
-}
-
-const URLConfig: IURLConfig = {
-  baseURL: baseURL,
-  loginURL: baseURL + "/login",
-};
 
 export default URLConfig;
+
+const urlConfig = new URLConfig();
+export const baseURL = urlConfig.getBaseURL();
+export const loginURL = baseURL + "/login";
